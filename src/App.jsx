82a81bcaee2e163de0536979195ad26fd0bde4de
leabentancor <leabentancor@icloud.com>
2024-05-16 23:59:41 -0300

    correccion del main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import ContenedorLista from "./componentes/ContenedorLista"
 import { Header } from "./componentes/header/Header"
 import "./css/style.css"
-import "./main.jsx"
 
 function App() {
 
