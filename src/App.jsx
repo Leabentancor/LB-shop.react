cdce35861bf754106cc335385255fc1af64bf4fc
leabentancor <leabentancor@icloud.com>
2024-05-16 23:49:40 -0300

    segundo arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ContenedorLista from "./componentes/ContenedorLista"
 import { Header } from "./componentes/header/Header"
 import "./css/style.css"
+import "./main.jsx"
 
 function App() {
 
